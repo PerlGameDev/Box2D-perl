e512b29020acb36a7665e8d3101ee5be612e59da
kthakore <thakore.kartik@gmail.com>
2011-05-04 23:58:52 -0400

    Added type and constants for b2Shape

--- lib/Box2D.pm
+++ lib/Box2D.pm
@@ -20,9 +20,17 @@ our @EXPORT_OK   = ();
 our %EXPORT_TAGS = ( );
 
 use constant {
+#b2Body Type 
 b2_staticBody => 0,
 b2_kinematicBody => 1,
-b2_dynamicBody => 2
+b2_dynamicBody => 2,
+
+#b2Shape Type 
+e_unknown=> -1,
+e_circle => 0,
+e_polygon => 1,
+e_typeCount => 2,
+
 };
 
 1; # End of Box2D
