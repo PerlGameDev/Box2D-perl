84e878a4f327070f0517d3188f603c5f961729fc
kthakore <thakore.kartik@gmail.com>
2011-05-16 16:48:37 -0400

    Made the tool spit out pod

--- tools/doxy_pod_con.pl
+++ tools/doxy_pod_con.pl
@@ -6,13 +6,30 @@ my $c_file_path = $ARGV[0];
 die "Need a c++ file to read" unless $c_file_path;
 
 open(my $FH, '<', $c_file_path);
-
-while(<$FH>)
-{
-#	chomp;
-
-	print "$1 \n $4 \n\n" if( $_ =~ /([\/]+[^\n]+)+\n(.+)/m );
+my $file = '';
+while(<$FH>) {
+    $file .= $_;
 }
-
 close $FH;
 
+
+my @matches = $file =~ /(\/\/\/+[^\n]+)+\n([^\n]+\s[^\n]+;)/gm;
+
+my $comments;
+my $function;
+foreach(@matches)
+{
+	if( $_ =~ /(\/\/\/)(.*)[^\n]/ )
+	{
+		$comments .= $2."\n";	
+	}
+	else
+	{
+		$function = $_;
+
+		print "=head2 $function \n$comments \n";
+
+		$comments = '';
+
+	}
+}
