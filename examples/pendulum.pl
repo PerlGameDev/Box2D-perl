9c258b741eb74263b716c68ecf0644c48510ce03
Zach Morgan <zpmorgan@gmail.com>
2011-07-09 04:57:57 -0400

    polypendulum upgrade

--- examples/pendulum.pl
+++ examples/pendulum.pl
@@ -6,9 +6,11 @@ use SDL;
 use SDL::Video;
 use SDLx::App;
 
+my $precision = 10;
+
 # pixels
-my $width  = 300;
-my $height = 300;
+my $width  = 450;
+my $height = 450;
 
 # pixels per meter
 my $ppm = 30;
@@ -18,7 +20,7 @@ my $mpp = 1.0 / $ppm;
 
 # frames per second
 my $fps      = 60.0;
-my $timestep = 1.0 / $fps;
+my $timestep = 1.0 / ($fps*$precision);
 
 # velocity iterations
 my $vIters = 10;
@@ -26,10 +28,12 @@ my $vIters = 10;
 # position iterations
 my $pIters = 10;
 
-my $gravity = Box2D::b2Vec2->new( 0, -10.0 );
-my $world = Box2D::b2World->new( $gravity, 1 );
+my $gravity = Box2D::b2Vec2->new( 0, -8.0 * $precision );
+my $world = Box2D::b2World->new( $gravity, 0 ); #no sleep. don't lose energy.
 
 my $rodColor = 0x00CC70FF;
+
+my $segments = $ARGV[0] || 2;
 
 my $pivot = {
     x0     => s2w( $width / 2 ),
@@ -40,21 +44,31 @@ my $pivot = {
 $pivot->{body}   = make_static_circle( @$pivot{qw( x0 y0 radius )} );
 $pivot->{anchor} = Box2D::b2Vec2->new( @$pivot{qw( x0 y0 )} );
 
-my $bob = {
-    x0     => s2w( $width - 40 ),
-    y0     => s2w( $height / 2 ),
-    radius => s2w(10),
-    color  => 0xCC005CFF,
-};
-$bob->{body}   = make_dynamic_circle( @$bob{qw( x0 y0 radius )} );
-$bob->{anchor} = Box2D::b2Vec2->new( @$bob{qw( x0 y0 )} );
+my $prev_pivot = $pivot;
+my @bobs;
+for (1..$segments){
+      
+   my $bob = {
+       y0     => s2w( $height /2 + .9*$height*$_/(2*$segments) ),
+       x0     => s2w( $width / 2 +1),
+       radius => s2w(6),
+       color  => 0xCC005CFF,
+   };
+   $bob->{body}   = make_dynamic_circle( @$bob{qw( x0 y0 radius )} );
+   $bob->{anchor} = Box2D::b2Vec2->new( @$bob{qw( x0 y0 )} );
 
-my $jointDef = Box2D::b2DistanceJointDef->new();
-$jointDef->Initialize( $pivot->{body}, $bob->{body}, $pivot->{anchor},
-    $bob->{anchor} );
-$jointDef->frequencyHz(4.0);
-$jointDef->dampingRatio(0.5);
-$world->CreateJoint($jointDef);
+   my $jointDef = Box2D::b2DistanceJointDef->new();
+   $jointDef->Initialize( $prev_pivot->{body}, $bob->{body}, $prev_pivot->{anchor},
+       $bob->{anchor} );
+   #high frequency means less energy lost from joint correction
+   $jointDef->frequencyHz($fps*$precision);
+   $jointDef->dampingRatio(0);
+   $world->CreateJoint($jointDef);
+   
+   $prev_pivot = $bob;
+   push @bobs, $bob;
+}
+
 
 my $app = SDLx::App->new(
     width  => $width,
@@ -73,17 +87,25 @@ $app->add_show_handler(
     sub {
         $world->Step( $timestep, $vIters, $pIters );
         $world->ClearForces();
-
-        my $p1 = $pivot->{body}->GetPosition();
-        my $p2 = $bob->{body}->GetPosition();
-
+        
         # clear surface
         $app->draw_rect( undef, 0x000000FF );
 
+        #draw 1st pendulum
+        my $p1 = $pivot->{body}->GetPosition();
+        my $p2 = $bobs[0]->{body}->GetPosition();
         $app->draw_line( [ w2s( $p1->x ), w2s( s2w($height) - $p1->y ) ],
             [ w2s( $p2->x ), w2s( s2w($height) - $p2->y ) ], $rodColor );
+        
+        #draw the other pendulums
+        for (0..$#bobs-1){
+           my $b1 = $bobs[$_]->{body}->GetPosition();
+           my $b2 = $bobs[$_+1]->{body}->GetPosition();
+            $app->draw_line( [ w2s( $b1->x ), w2s( s2w($height) - $b1->y ) ],
+                [ w2s( $b2->x ), w2s( s2w($height) - $b2->y ) ], $rodColor );
+        }
         draw_circle($pivot);
-        draw_circle($bob);
+        draw_circle($_) for @bobs;
 
         if ( $frames % $fps == 0 ) {
             my $t = SDL::get_ticks();
@@ -116,9 +138,7 @@ sub make_static_circle {
 
     my $circle = Box2D::b2CircleShape->new();
     $circle->m_radius($r);
-
-    $body->CreateFixture( $circle, 0.0 );
-
+ 
     return $body;
 }
 
@@ -127,17 +147,17 @@ sub make_dynamic_circle {
 
     my $bodyDef = Box2D::b2BodyDef->new();
     $bodyDef->type(Box2D::b2_dynamicBody);
+    $bodyDef->linearDamping(0.0);
+    $bodyDef->angularDamping(0.0);
+    
     $bodyDef->position->Set( $x, $y );
-    $bodyDef->linearDamping(0.1);
-    $bodyDef->angularDamping(0.1);
     my $body = $world->CreateBody($bodyDef);
-
-    my $circle = Box2D::b2CircleShape->new();
-    $circle->m_radius($r);
-
-    $body->CreateFixture( $circle, 1.0 );
-
+     
+    my $dynamicCircle = Box2D::b2CircleShape->new();
+    $dynamicCircle->m_radius(8);
+     
     return $body;
+      
 }
 
 sub draw_circle {
