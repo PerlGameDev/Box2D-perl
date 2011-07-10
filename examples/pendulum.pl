8c70e6df66fdb1e321b57a8a4a166207d7fdaa22
Jeffrey T. Palmer <jeffrey.t.palmer@gmail.com>
2011-07-10 16:46:17 -0400

    Tidied

--- examples/pendulum.pl
+++ examples/pendulum.pl
@@ -8,7 +8,6 @@ use SDLx::App;
 
 my $segments = $ARGV[0] || 2;
 
- 
 # pixels
 my $width  = 450;
 my $height = 450;
@@ -24,15 +23,17 @@ my $fps      = 60.0;
 my $timestep = 1.0 / $fps;
 
 # velocity iterations
-my $vIters = 50000/$segments;
+my $vIters = 50000 / $segments;
 
 # position iterations
-my $pIters = 50000/$segments;
+my $pIters = 50000 / $segments;
 
 my $gravity = Box2D::b2Vec2->new( 0, -8.0 );
-my $world = Box2D::b2World->new( $gravity, 0 ); #no sleep. don't lose energy.
 
-my $rodColor = 0x00CC70FF;
+# no sleep. don't lose energy.
+my $world = Box2D::b2World->new( $gravity, 0 );
+
+my $rodColor  = 0x00CC70FF;
 my $pathColor = 0xFFFFCFFF;
 
 my $pivot = {
@@ -46,29 +47,31 @@ $pivot->{anchor} = Box2D::b2Vec2->new( @$pivot{qw( x0 y0 )} );
 
 my $prev_pivot = $pivot;
 my @bobs;
-for (1..$segments){
-      
-   my $bob = {
-       y0     => s2w( $height /2 + .9*$height*$_/(2*$segments) ),
-       x0     => s2w( $width / 2 +1),
-       radius => s2w(6),
-       color  => 0xCC005CFF,
-   };
-   $bob->{body}   = make_dynamic_circle( @$bob{qw( x0 y0 radius )} );
-   $bob->{anchor} = Box2D::b2Vec2->new( @$bob{qw( x0 y0 )} );
+for ( 1 .. $segments ) {
 
-   my $jointDef = Box2D::b2DistanceJointDef->new();
-   $jointDef->Initialize( $prev_pivot->{body}, $bob->{body}, $prev_pivot->{anchor},
-       $bob->{anchor} );
-   #high frequency means less energy lost from joint correction
-   $jointDef->frequencyHz(1/$timestep);
-   $jointDef->dampingRatio(0);
-   $world->CreateJoint($jointDef);
-   
-   $prev_pivot = $bob;
-   push @bobs, $bob;
+    my $bob = {
+        y0 => s2w( $height / 2 + .9 * $height * $_ / ( 2 * $segments ) ),
+        x0 => s2w( $width / 2 + 1 ),
+        radius => s2w(6),
+        color  => 0xCC005CFF,
+    };
+    $bob->{body}   = make_dynamic_circle( @$bob{qw( x0 y0 radius )} );
+    $bob->{anchor} = Box2D::b2Vec2->new( @$bob{qw( x0 y0 )} );
+
+    my $jointDef = Box2D::b2DistanceJointDef->new();
+    $jointDef->Initialize(
+        $prev_pivot->{body},   $bob->{body},
+        $prev_pivot->{anchor}, $bob->{anchor}
+    );
+
+    #high frequency means less energy lost from joint correction
+    $jointDef->frequencyHz( 1 / $timestep );
+    $jointDef->dampingRatio(0);
+    $world->CreateJoint($jointDef);
+
+    $prev_pivot = $bob;
+    push @bobs, $bob;
 }
-
 
 my $app = SDLx::App->new(
     width  => $width,
@@ -79,11 +82,12 @@ my $app = SDLx::App->new(
     eoq    => 1,
 );
 my $bg = $app->duplicate;
-$bg->draw_rect([0,0,$width,$height],[0,0,0,255]);
+$bg->draw_rect( [ 0, 0, $width, $height ], [ 0, 0, 0, 255 ] );
 
 my $realFps = $fps;
 my $frames  = 1;
 my $ticks   = SDL::get_ticks();
+
 #last location of end of pendulum
 my $prev_path_pos;
 
@@ -91,27 +95,30 @@ $app->add_show_handler(
     sub {
         $world->Step( $timestep, $vIters, $pIters );
         $world->ClearForces();
-        
+
         my $endpoint = $bobs[$#bobs]{body}->GetPosition();
-        my $current_path_pos = [ w2s( $endpoint->x ), w2s( s2w($height) - $endpoint->y ) ];
+        my $current_path_pos
+            = [ w2s( $endpoint->x ), w2s( s2w($height) - $endpoint->y ) ];
+
         #trace path on bg
         $bg->draw_line( $prev_path_pos, $current_path_pos, $pathColor )
             if $prev_path_pos;
-        $prev_path_pos = $current_path_pos;    
-        
+        $prev_path_pos = $current_path_pos;
+
         # draw bg
-        $bg->blit( $app, [0,0,$width,$height]);
+        $bg->blit( $app, [ 0, 0, $width, $height ] );
+
         #$app->draw_rect([0,0,$width,$height],[0,0,0,255]);
         #draw 1st pendulum
         my $p1 = $pivot->{body}->GetPosition();
         my $p2 = $bobs[0]->{body}->GetPosition();
         $app->draw_line( [ w2s( $p1->x ), w2s( s2w($height) - $p1->y ) ],
             [ w2s( $p2->x ), w2s( s2w($height) - $p2->y ) ], $rodColor );
-        
+
         #draw the other pendulums
-        for (0..$#bobs-1){
-           my $b1 = $bobs[$_]->{body}->GetPosition();
-           my $b2 = $bobs[$_+1]->{body}->GetPosition();
+        for ( 0 .. $#bobs - 1 ) {
+            my $b1 = $bobs[$_]->{body}->GetPosition();
+            my $b2 = $bobs[ $_ + 1 ]->{body}->GetPosition();
             $app->draw_line( [ w2s( $b1->x ), w2s( s2w($height) - $b1->y ) ],
                 [ w2s( $b2->x ), w2s( s2w($height) - $b2->y ) ], $rodColor );
         }
@@ -149,7 +156,7 @@ sub make_static_circle {
 
     my $circle = Box2D::b2CircleShape->new();
     $circle->m_radius($r);
- 
+
     return $body;
 }
 
@@ -160,15 +167,14 @@ sub make_dynamic_circle {
     $bodyDef->type(Box2D::b2_dynamicBody);
     $bodyDef->linearDamping(0.0);
     $bodyDef->angularDamping(0.0);
-    
+
     $bodyDef->position->Set( $x, $y );
     my $body = $world->CreateBody($bodyDef);
-     
+
     my $dynamicCircle = Box2D::b2CircleShape->new();
     $dynamicCircle->m_radius(8);
-     
+
     return $body;
-      
 }
 
 sub draw_circle {
