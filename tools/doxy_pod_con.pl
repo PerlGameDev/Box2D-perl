3043a7fdda8b8257ec71a281762ba3af2ac7aa64
Jeffrey T. Palmer <jeffrey.t.palmer@gmail.com>
2011-07-18 19:50:13 -0400

    Added pod template

--- tools/doxy_pod_con.pl
+++ tools/doxy_pod_con.pl
@@ -1,44 +1,93 @@
+#!/usr/bin/env perl
 use strict;
 use warnings;
+use Template;
+use File::Slurp qw(read_file);
 
-my $c_file_path = $ARGV[0];
+sub main {
+    my ($path) = @_;
 
-die "Need a c++ file to read" unless $c_file_path;
+    die "Need an html file to read\n" unless defined $path;
+    die "Not a file: $path\n" unless -f $path;
 
-open(my $FH, '<', $c_file_path);
-my $file = '';
-while(<$FH>) {
-    $file .= $_;
+    my $file  = read_file($path);
+    my $class = parse_html($file);
+    output_pod($class);
 }
-close $FH;
 
+sub parse_html {
+    my ($html) = @_;
 
-my @matches = $file =~ /(\/\/\/+[^\n]+)+\n([^\n]+\s[^\n]+;)/gm;
+    my $class = {};
 
-my $comments;
-my $function;
-foreach(@matches)
-{
-	chomp;
-	$_ =~ s/\\m//g;
-	print $_."\n";
+    my %re = (
+        name        => qr|<title>Box2D:\ (\w+)\ Class\ Reference</title>|xms,
+        description => qr|<h2>Detailed\ Description</h2>\s*<p>(.*?)</p>|xms,
+    );
+
+    while ( my ( $key, $regex ) = each %re ) {
+        if ( $html =~ $regex ) {
+            $class->{$key} = $1;
+        }
+        else {
+            $class->{$key} = 'TODO';
+            warn "Could not find $key\n";
+        }
+    }
+
+    # Use the first sentence from the description for the abstract
+    if ( $class->{description} =~ /^((?:.*?)\.)/xs ) {
+        $class->{abstract} = $1;
+    }
+    else {
+        $class->{abstract} = 'TODO';
+    }
+
+    $class->{methods} = [];
+
+    return $class;
+}
+
+sub output_pod {
+    my ($class) = @_;
+    my $tt = Template->new() or die "$Template::ERROR\n";
+    $tt->process( \*DATA, $class ) or die $tt->error(), "\n";
+}
+
+main(@ARGV);
+
+__END__
 
 =pod
-	if( $_ =~ /(\/\/\/)(.*)/ )
-	{
-		$comments .= $2."\n";	
-	}
-	else
-	{
-		if( $comments )
-		{
-		$function = $_;
 
-		print "=head2 $function \n$comments \n";
+=head1 NAME
 
-		$comments = '';
-		}
+Box2D::[% name %] - [% abstract %]
 
-	}
-=cut 
-}
+=head1 SYNOPSIS
+
+  # TODO
+
+=head1 DESCRIPTION
+
+[% description %]
+
+=head1 METHODS
+[% FOREACH method = methods %]
+=head2 [% method.name %]
+
+[% method.description %]
+[% END %]
+=head1 BUGS
+
+=head1 AUTHORS
+
+See L<Box2D/AUTHORS>
+
+=head1 COPYRIGHT & LICENSE
+
+=head1 SEE ALSO
+
+[% see_also %]
+
+=cut
