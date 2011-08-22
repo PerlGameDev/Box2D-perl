4e0caa546bd09fc6c18416ee90b79fa324274275
Jeffrey T. Palmer <jeffrey.t.palmer@gmail.com>
2011-08-21 20:45:40 -0400

    Tidied helper.h

--- src/helper.h
+++ src/helper.h
@@ -1,29 +1,24 @@
 #ifndef __HELPER_H__
 #define __HELPER_H__
 
-void * stack_to_object( SV* arg )
+void* stack_to_object( SV* arg )
 {
+	void* var = NULL;
 
-	void * var = NULL;
-	if( sv_isobject(arg) && (SvTYPE(SvRV(arg)) == SVt_PVMG) )
+	if ( sv_isobject(arg) && ( SvTYPE(SvRV(arg)) == SVt_PVMG ) )
 	{
-		var = (void *)SvIV((SV*)SvRV( arg ));
+		var = (void*)SvIV( (SV*)SvRV(arg) );
 	}
 
-
 	return var;
-
 }
-
-
 
 SV* new_data( SV* thing )
 {
- if (  SvROK( thing ) ) 
-    return  newRV_inc(SvRV(thing ) );
- else
-    return  SvREFCNT_inc(thing); 
-
+	if ( SvROK(thing) )
+		return  newRV_inc( SvRV(thing) );
+	else
+		return  SvREFCNT_inc(thing);
 }
 
 #endif
