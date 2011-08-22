1d66fa8dfb6c0f6d01d99d313d8c66cdf466a538
Jeffrey T. Palmer <jeffrey.t.palmer@gmail.com>
2011-08-22 19:39:48 -0400

    Remove unneeded check

--- src/PerlRayCastCallback.h
+++ src/PerlRayCastCallback.h
@@ -20,33 +20,30 @@ public:
 	virtual float32 ReportFixture( b2Fixture *fixture, const b2Vec2 &point, const b2Vec2 &normal, float32 fraction )
 	{
 		float32 rv = 0.0;
+		int count;
 
-		if (reportFixture) {
-			int count;
+		dSP;
+		ENTER;
+		SAVETMPS;
+		PUSHMARK(SP);
 
-			dSP;
-			ENTER;
-			SAVETMPS;
-			PUSHMARK(SP);
+		XPUSHs( sv_2mortal(object_to_stack( sizeof(b2Fixture*), (void*)fixture, "Box2D::b2Fixture" )) );
+		XPUSHs( sv_2mortal(object_to_stack( sizeof(b2Vec2*),    (void*)&point,  "Box2D::b2Vec2" )) );
+		XPUSHs( sv_2mortal(object_to_stack( sizeof(b2Vec2*),    (void*)&normal, "Box2D::b2Vec2" )) );
+		XPUSHs( sv_2mortal(newSVnv(fraction)) );
 
-			XPUSHs( sv_2mortal(object_to_stack( sizeof(b2Fixture*), (void*)fixture, "Box2D::b2Fixture" )) );
-			XPUSHs( sv_2mortal(object_to_stack( sizeof(b2Vec2*),    (void*)&point,  "Box2D::b2Vec2" )) );
-			XPUSHs( sv_2mortal(object_to_stack( sizeof(b2Vec2*),    (void*)&normal, "Box2D::b2Vec2" )) );
-			XPUSHs( sv_2mortal(newSVnv(fraction)) );
+		PUTBACK;
 
-			PUTBACK;
+		count = call_sv( reportFixture, G_SCALAR );
+		SPAGAIN;
 
-			count = call_sv( reportFixture, G_SCALAR );
-			SPAGAIN;
-
-			if ( count != 0 ) {
-				rv = POPn;
-			}
-
-			PUTBACK;
-			FREETMPS;
-			LEAVE;
+		if ( count != 0 ) {
+			rv = POPn;
 		}
+
+		PUTBACK;
+		FREETMPS;
+		LEAVE;
 
 		return rv;
 	}
